69c6f1bda62ebcc7016b6c24c6451e016a7994eb
[Nyamburavee] <[veronyambura716@gmail.com]>
2024-07-25 20:55:29 +0300

    click counter timer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {useState, useEffect, useRef} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App() {
+    const[count, setCount] = useState(0)
+    const [timer, setTimer] = useState(10)
+    const id = useRef(null)
+    
+    const clear = () => window.clearInterval(id.current)
+    
+    useEffect(() =>{
+        id.current = window.setInterval(() =>{
+            setTimer((time) => time -1)
+        },1000)
+        
+        return clear
+    }, [])
+    
+   useEffect(() =>{
+       if(timer === 0){
+           clear()
+       }
+   }, [timer])
+    
+    return(
+        <div className="container">
+        <h1>{count} Clicks</h1>
+        <h3>Time left: {timer} Seconds</h3>
+        {timer === 0 ? null :
+        <button onClick={() => setCount((c) => c + 1)}>+</button>}
+        </div>
+    )
 }
 
-export default App;
